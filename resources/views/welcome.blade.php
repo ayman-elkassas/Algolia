2cb7003380ca3b3d78f0d05f72a2b02f58af01fb
Ayman Elkassas <aymanelkassas88@gmail.com>
2020-05-26 00:36:25 +0200

    done demo algolia search engine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,55 @@
 @section('content')
     <div class="container">
 
+{{--        config for search--}}
         <ais-index
             app-id="9UDWIDWIPT"
             api-key="b74485d25a02461e9d9e43eaf75fb715"
             index-name="people">
 
-            <ais-search-box></ais-search-box>
-            <ais-results>
+{{--            search engine input--}}
 
-                <template scope="{result}">
-                    @{{ result.first_name }}
-                </template>
+            <ais-search-box
+            placeholder="Search..."
+            submit-title="Search"
+            reset-title="string"
+            :autofocus="boolean"
+            :show-loading-indicator="boolean"
+            :class-names="object"
+            ></ais-search-box>
+
+            {{--            result can preview from scope result and fetch as following--}}
+            <ais-results class="row">
+                    <template scope="{result}" >
+
+                        <div class="col-12 col-md-4 mb-4">
+                            <div class="card">
+                                <img :src="result.photo" class="card-img-top" alt="Card img cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">@{{ result.first_name }} @{{ result.last_name }}</h5>
+                                    <p class="card-text">
+                                        <strong>@{{ result.title }}</strong>
+                                        <em>@{{ result.email }}</em>
+                                        <address>
+                                        <strong>@{{ result.company }}</strong>
+                                        @{{ result.address }}<br>
+                                        @{{ result.address_2 }} <br>
+                                        @{{ result.city }}, @{{ result.state }} @{{ result.zip_code }}<br>
+                                        <abbr title="phone">P:</abbr> @{{ result.phone }}
+                                        </address>
+                                    </p>
+                                    <a :href="result.id" class="btn btn-primary">View</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </template>
+
 
             </ais-results>
 
         </ais-index>
+
 
     </div>
 @endsection
